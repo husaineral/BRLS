79020df5bf72bfd1c22d8ea26ae1bde5077c1973
husai <husain.neralwala@gmail.com>
2020-10-07 08:02:06 +0530

    automated test cases for bug 1 and bug 2

---
+++ tests/library/entities/LibraryTest.java
@@ -0,0 +1,42 @@
+package library.entities;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LibraryTest {
+    private double FINE_PER_DAY=1.0;
+    private Date dueDate;
+
+    @Test
+void testCalculateOverDueFine()
+    {
+        double fine = 0.0;
+        Date currentDate=Calendar.getInstance().getDate();
+        dueDate=new Date ( currentDate.getTime() + TimeUnit.DAYS.toMillis(-1));
+        long daysOverDue = Calendar.getInstance().getDaysDifference(dueDate);
+        fine = daysOverDue * FINE_PER_DAY;
+        assertNotEquals(1,fine);
+    }
+
+
+    @Test
+    void testCalculateOverDueFineIsHalf()
+    {
+        double fine = 0.0;
+        Date currentDate=Calendar.getInstance().getDate();
+        dueDate=new Date ( currentDate.getTime() + TimeUnit.DAYS.toMillis(-4));
+        System.out.println("Current Date :+"+ currentDate);
+        System.out.println("Due Date :"+ dueDate);
+
+        long daysOverDue = Calendar.getInstance().getDaysDifference(dueDate);
+        fine = daysOverDue * FINE_PER_DAY;
+        System.out.println("Expected Fine $4" );
+        System.out.println("Actual Fine $  " +fine);
+        assertNotEquals(4,fine);
+    }
+}
+
